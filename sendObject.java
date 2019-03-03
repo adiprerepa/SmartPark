47a3fb7a7e4d66495a00f4247e7049c173f08380
Ishan Jain <ishanjain42@gmail.com>
2019-03-02 18:57:44 -0800

    Add files via upload
    adi

---
+++ sendObject.java
@@ -0,0 +1,10 @@
+public class sendObject {
+
+    private String type;
+    private String time;
+
+    public sendObject(String type, String time) {
+        this.type = type;
+        this.time = time;
+    }
+}
